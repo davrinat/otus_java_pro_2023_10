b929010b522a82b638df5b453fdfa321d6656c84
davrinat <davrinat@gmail.com>
2024-01-05 17:42:03 +0300

    hw08-first_commit

---
+++ hw08-IO-streams/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -0,0 +1,25 @@
+package ru.otus.dataprocessor;
+
+import com.google.gson.Gson;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class FileSerializer implements Serializer {
+    private final String fileName;
+
+    public FileSerializer(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void serialize(Map<String, Double> data) {
+        // формирует результирующий json и сохраняет его в файл
+        try (var writer = new FileWriter(fileName)) {
+            new Gson().toJson(data, writer);
+        } catch (IOException e) {
+            throw new FileProcessException(e.getMessage());
+        }
+    }
+}
